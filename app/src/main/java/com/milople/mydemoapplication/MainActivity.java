bb80d0ee419c46a3a8b2773a20100a2167d1bf2d
Shivani <shivanigosaliya1998@gmail.com>
2022-01-04 11:20:13 +0530

    commit a

--- app/src/main/java/com/milople/mydemoapplication/MainActivity.java
+++ app/src/main/java/com/milople/mydemoapplication/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("jsdjf","dkf");
         Init();
         HandleEvent();
         Toastermessage.toasty(this,"HELLO");
